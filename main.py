fc702b5dcabd07886e36ed9051d2cff26faee3fc
Jacek programmer <jacek301@gmail.com>
2025-09-11 01:39:43 +0200

    redesign Gui: make kivy refresh layout by force it to refresh and calculate it positions again. This fix problem with no clickable region of spinner at first application start. Make not nesesery to manually resize window.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.core.window import Window
 
 from interest_rate_input_view import InterestRateInput
@@ -14,11 +15,22 @@ Window.size = (760, 450)
 class PlotMakerApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.original_size = None
         self._ = set_language('pl')
         # self._ = set_language('en')
 
     def build(self):
+        self.original_size = Window.size
+        Clock.schedule_once(self.refresh_window_layout, 0.1)
         return InterestRateInput()
+
+    def refresh_window_layout(self, dt):
+        Window.size = (self.original_size[0] + 1, self.original_size[1] + 1)
+
+        Clock.schedule_once(self.restore_window_size, 0.1)
+
+    def restore_window_size(self, dt):
+        Window.size = self.original_size
 
 
 if __name__ == "__main__":
