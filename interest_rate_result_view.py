f9c6147358c02dcf26d5f8bc63843b8db410879d
Jacek programmer <jacek301@gmail.com>
2025-10-13 00:18:17 +0200

    language switch: translation in interest_rate_result_view completed.

--- interest_rate_result_view.py
+++ interest_rate_result_view.py
@@ -7,6 +7,28 @@ Builder.load_file("interest_rate_result_view.kv")
 
 
 class InterestRateResultView(BoxLayout):
+    LABEL_TEXTS = {
+        "interest_result_title_label_id": "[u]Deposit (zł)[/u]",
+        "deposit_amount_label_id": "Deposit (zł):",
+        "deposit_time_months_label_id": "Time (m.):",
+        "bank_interest_rate_label_id": "Percent (%):",
+        "capital_with_interest_gross_label_id": "Gross profit",
+        "capital_with_interest_net_label_id": "Net profit",
+        "interest_rate_gross_label_id": "Gross interest",
+        "interest_rate_net_label_id": "Net interest",
+        "tax_from_interest_rate_label_id": "Tax",
+        "gross_daily_interest_rate_label_id": "Gross daily",
+        "net_daily_interest_rate_label_id": "Net daily",
+        "tax_daily_interest_rate_label_id": "Tax daily",
+        "gross_monthly_interest_rate_label_id": "Gross monthly",
+        "net_monthly_interest_rate_label_id": "Net monthly",
+        "tax_monthly_interest_rate_label_id": "Tax monthly",
+        "gross_annual_interest_rate_label_id": "Gross annual",
+        "net_annual_interest_rate_label_id": "Net annual",
+        "tax_annual_interest_rate_label_id": "Tax annual",
+
+    }
+
     depositAmount = StringProperty("")
     depositTimeMonths = StringProperty("")
     bankInterestRate = StringProperty("")
@@ -34,9 +56,7 @@ class InterestRateResultView(BoxLayout):
         return App.get_running_app()._(key)
 
     def refresh_texts(self):
-        labels_translations = {
-            "interest_result_title_id": self.translate("[u]Deposit (zł)[/u]"),
-        }
-        for widget_id, translated_text in labels_translations.items():
+        for widget_id, raw_text in self.LABEL_TEXTS.items():
+            translated_text = self.translate(raw_text)
             if widget_id in self.ids:
                 self.ids[widget_id].text = translated_text
